893405804cb987762b58af10cf600ab11242f3e6
Dima Hirchyk <dimzuk04@gmail.com>
2024-10-29 19:57:08 +0200

    1

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,8 @@
 function getShippingMessage(counry, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
-  console.log(
-    `"Shipping to ${counry} will cost ${totalPrice} credits"`
-  );
+  return `"Shipping to ${counry} will cost ${totalPrice} credits"`;
 }
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
+
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
